d07d6d5a11f0e9c884d475b545da58344cd01f63
Luis <lalvarezguillen@gmail.com>
2018-07-29 10:14:46 +0200

    extending unittests

--- proxy_test.go
+++ proxy_test.go
@@ -40,10 +40,10 @@ func (c *MockedSQS) DeleteMessage(i *sqs.DeleteMessageInput) (*sqs.DeleteMessage
 }
 
 func TestProxyMessages(t *testing.T) {
+	// Setup
 	c := &MockedSQS{}
 	d := []string{
 		"http://queues.com/dummy-destination",
-		// "http://queues.com/dummy-recipient",
 	}
 
 	i := sqs.ReceiveMessageInput{QueueUrl: aws.String("http://queues.com/dummy")}
@@ -77,14 +77,52 @@ func TestProxyMessages(t *testing.T) {
 	}
 	c.On("DeleteMessage", &dmInput2).Return(&sqs.DeleteMessageOutput{}, nil)
 
+	// Actual test
 	proxyMessages(c, &i, d)
-
-	// c.AssertExpectations(t)
-	// c.AssertCalled(t, "ReceiveMessage")
-	// c.AssertCalled(t, "SendMessage")
-	// c.AssertCalled(t, "DeleteMessage")
+	c.AssertExpectations(t)
+	c.AssertNumberOfCalls(t, "ReceiveMessage", 1)
+	c.AssertNumberOfCalls(t, "SendMessage", 2)
+	c.AssertNumberOfCalls(t, "DeleteMessage", 2)
 }
 
-// func TestNoMessagesToProxy(t *testing.T){
+func TestNoMessagesToProxy(t *testing.T) {
+	// Setup
+	c := &MockedSQS{}
+	d := []string{
+		"http://queues.com/dummy-destination",
+	}
 
-// }
+	i := sqs.ReceiveMessageInput{QueueUrl: aws.String("http://queues.com/dummy")}
+	outp := sqs.ReceiveMessageOutput{
+		Messages: []*sqs.Message{},
+	}
+	c.On("ReceiveMessage", &i).Return(outp, nil)
+
+	smInput1 := sqs.SendMessageInput{
+		MessageBody: aws.String("dummy message 1"),
+		QueueUrl:    aws.String(d[0]),
+	}
+	c.On("SendMessage", &smInput1).Return(&sqs.SendMessageOutput{}, nil)
+	smInput2 := sqs.SendMessageInput{
+		MessageBody: aws.String("dummy message 2"),
+		QueueUrl:    aws.String(d[0]),
+	}
+	c.On("SendMessage", &smInput2).Return(&sqs.SendMessageOutput{}, nil)
+
+	dmInput1 := sqs.DeleteMessageInput{
+		QueueUrl:      i.QueueUrl,
+		ReceiptHandle: aws.String("dummy-1"),
+	}
+	c.On("DeleteMessage", &dmInput1).Return(&sqs.DeleteMessageOutput{}, nil)
+	dmInput2 := sqs.DeleteMessageInput{
+		QueueUrl:      i.QueueUrl,
+		ReceiptHandle: aws.String("dummy-2"),
+	}
+	c.On("DeleteMessage", &dmInput2).Return(&sqs.DeleteMessageOutput{}, nil)
+
+	// Actual test
+	proxyMessages(c, &i, d)
+	c.AssertNumberOfCalls(t, "ReceiveMessage", 1)
+	c.AssertNotCalled(t, "SendMessage")
+	c.AssertNotCalled(t, "DeleteMessage")
+}
